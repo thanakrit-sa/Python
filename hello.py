0df02d7529bf0a1be01934d8173dbd1d104f41c7
thanakrit <iamsuttichai@Macbook-iamsuttichai.local>
2020-04-12 19:23:56 +0700

    now

---
+++ hello.py
@@ -0,0 +1,12 @@
+# name = input('What is your name?\n')
+# print ('Hi, %s.' % name)
+# print ('Welcome to Python Yeah.')
+
+# number = int(input("Enter your number : "))
+# if number > 10:
+#     print("Number > 10")
+# else:
+#     print("Number < 10")
+# for i in range(number):
+#     print(i)
+
